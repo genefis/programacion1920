83e182b8c28aa823b8c382fa6ffa9b3447d94b2b
genefis <genesisgonzalez492@gmail.com>
2019-11-15 19:09:40 -0500

    clase 6

---
+++ introJava/src/introjava/Acumuladores.java
@@ -0,0 +1,19 @@
+
+package introjava;
+
+
+public class Acumuladores {
+    public static void main(String[] args) {
+        System.out.println("Trbajando con acumuladores");
+        int contador =1;
+        int acumulador=1;
+        while(contador <=5){
+            acumulador = acumulador + contador;
+            
+               
+            contador++;
+        }
+         System.out.println(+contador);
+        System.out.println(+acumulador);
+    }
+}
---
+++ introJava/src/introjava/Contador.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package introjava;
+
+/**
+ *
+ * @author Salas
+ */
+public class Contador {
+    public static void main(String[] args) {
+        System.out.println("trabajando en contadores");
+        // Inicializar nuestro contador
+        int contador = 1;
+        // vamos a repetir 5 veces un proceos
+        while ( contador <= 10){
+            System.out.println("contador."+contador);
+            contador++;
+           
+        }
+            
+    }
+}
