50b16d628fff666015070124c6bc8f78a6de3eae
genefis <genesisgonzalez492@gmail.com>
2020-01-02 20:17:20 -0500

    clase

--- introJava/src/tarea_2_1_2020/Binario_decimal.java
+++ introJava/src/tarea_2_1_2020/Binario_decimal.java
@@ -6,7 +6,7 @@ public class Binario_decimal {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int termino = 0;
+
         int suma = 0;
         int bin = 0;
 
@@ -20,13 +20,15 @@ public class Binario_decimal {
 
         for (int i = binario.length - 1; i >= 0; i--) {
             bin = (binario[i] * 2 ^ i);
-            termino = termino + bin;
-            System.out.print("El termino ingresado es: " + binario[i] + ":la poscion es:" + i);
-System.out.println("");
-        }
+            suma = suma + bin;
 
-        
-        System.out.print("El numero decimal es:" + termino);
+        }
+        for (int i = binario.length - 1; i >= 0; i--) {
+            System.out.print("El termino ingresado es: " + binario[i] + ":la poscion es:" + i);
+            System.out.println("");
+        }
+        System.out.print("El numero decimal es:" + bin);
+        System.out.print("El numero decimal es:" + suma);
 
     }
 }
