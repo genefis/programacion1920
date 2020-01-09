c2c6ec6c8f8aecf6782b22cb1ae1317ff84cfd06
genefis <genesisgonzalez492@gmail.com>
2020-01-09 17:25:59 -0500

    tarea

---
+++ introJava/src/Tarea_9_1_2020/Tablas.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tarea_9_1_2020;
+
+/**
+ *
+ * @author Vanessa
+ */
+public class Tablas {
+    
+}
