c3e9ab59745d642048aeeb186636a9d88a10b71e
genefis <genesisgonzalez492@gmail.com>
2020-01-16 20:04:35 -0500

    d

---
+++ introJava/src/POO/Tiempo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package POO;
+
+/**
+ *
+ * @author UTPL
+ */
+public class Tiempo {
+    // Atributos de la clase, se declara de forma global
+    private int hota;
+    private int minuto;
+    private int segundo;
+    
+    /**
+     * Este metodo es el contructor de la clase
+     * El contructor se ejecuta al momento de crear un objeto para la clase tiempo
+     */
+    public Tiempo(){
+        System.out.println("Se iniciado la clase tiempo");
+    }
+    
+    
+}
